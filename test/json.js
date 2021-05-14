c176c15b96b96cc4c6cb703b6d021e52ad832fbb
skdi262 <skdi262@naver.com>
2021-05-14 16:49:00 +0900

    210514 7교시 커밋

---
+++ test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e127eb5e89bb793ebe0e3",
+      "age": 24,
+      "eyeColor": "green",
+      "name": "Aisha Keller"
+    },
+    {
+      "_id": "609e127e3db7f6ae5f006f7c",
+      "age": 30,
+      "eyeColor": "blue",
+      "name": "Kenya Mckenzie"
+    },
+    {
+      "_id": "609e127e2ff366ea92ef437b",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": "Tillman Hull"
+    },
+    {
+      "_id": "609e127e4e4776365ec04598",
+      "age": 26,
+      "eyeColor": "green",
+      "name": "Livingston Chaney"
+    },
+    {
+      "_id": "609e127e7edd4c1b37bd4f04",
+      "age": 32,
+      "eyeColor": "blue",
+      "name": "Kristina Woodward"
+    },
+    {
+      "_id": "609e127e11dea475aa20bce2",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Sarah Bird"
+    },
+    {
+      "_id": "609e127e1ab5b644460a7614",
+      "age": 32,
+      "eyeColor": "blue",
+      "name": "Janna Powell"
+    },
+    {
+      "_id": "609e127ef2994c22b8ded281",
+      "age": 26,
+      "eyeColor": "brown",
+      "name": "Chavez Ratliff"
+    },
+    {
+      "_id": "609e127e89e8c18a6265778c",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Helena Salinas"
+    },
+    {
+      "_id": "609e127ea313b4d23c7d2997",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Walls Valentine"
+    },
+    {
+      "_id": "609e127e7e4af949ed245d03",
+      "age": 27,
+      "eyeColor": "blue",
+      "name": "Miles Callahan"
+    },
+    {
+      "_id": "609e127e3395b8fb5001c7c1",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Harrell Goff"
+    },
+    {
+      "_id": "609e127e28047f467916f689",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": "Melinda Dorsey"
+    },
+    {
+      "_id": "609e127ed8fb625760a91668",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Haley Oneil"
+    },
+    {
+      "_id": "609e127ee8efdb54caf3303d",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Sanders Larson"
+    },
+    {
+      "_id": "609e127e82a8921e2ad52bf1",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Sandoval Dalton"
+    },
+    {
+      "_id": "609e127e355b5d154b9d5756",
+      "age": 27,
+      "eyeColor": "brown",
+      "name": "Jodi Rocha"
+    },
+    {
+      "_id": "609e127ec2159e7c206637fd",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Hancock Holman"
+    },
+    {
+      "_id": "609e127ef9bce1260966a71e",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Lindsay Spencer"
+    },
+    {
+      "_id": "609e127e49f02bc0caf4c31e",
+      "age": 23,
+      "eyeColor": "green",
+      "name": "Wooten Barrett"
+    }
+  ]
