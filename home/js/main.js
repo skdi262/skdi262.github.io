4ef1c0c631b3350cbdbe47276c8c380dc5ef2696
skdi262 <skdi262@naver.com>
2021-05-20 15:19:27 +0900

    dfa

--- home/js/main.js
+++ home/js/main.js
@@ -1,77 +1,77 @@
-$(document).ready(function() {
-	$('.to_top a').smoothScroll();
-	// 모바일용 메뉴 보이기 숨기기 액션 처리
+$(document).ready(function(){
+    $('.to_top a').smoothScroll();
+
+    // 모바일용 메뉴 보이기/숨기기 액션처리
 	$('.openMOgnb').click(function(){
-		// alert('메뉴 보이기 액션클릭');
+		// alert('메뉴보이기 액션클릭');
 		// $('.header_cont').css('display','block');
 		$('.header_cont').slideDown("slow");
 		$('#header').addClass('on');
 	});
-	$('.closePop').click(function() {
-		// $('.header_cont').css('display','none')
+	$('.closePop').click(function(){
+		// $('.header_cont').css('display','none');
 		$('.header_cont').slideUp("fast");
 		$('#header').removeClass('on');
 	});
 });
-// PC용 대메뉴 마우스 롤아웃 이벤트 처리 부분
-var isOver1 = false; //false 일 때 서브메뉴가 사라짐.
-var isOverSub1 = false; //서브메뉴 1번이 false 일 때 서브메뉴가 사라지는 조건
-// 1번 서브메뉴 숨기기
+
+// PC용 서브메뉴가 사라지는 액션(아래)
+var isOver1 = false; // 대메뉴1이 false 일때 서브메뉴가 사라지는 조건
+var isOverSub1 = false; // 서브메뉴1번이 false 일때 서브메뉴가 사라지는 조건
+// 1번 서브메뉴 숨기는 함수(아래)
 function goHide1() {
-	if(!isOver1 && !isOverSub1) {
+	if(!isOver1 && !isOverSub1) {//2개변수값이 false일때 구현조건이 됩니다.
 		$('.gnb_depth2_1').stop().fadeOut('fast');
-	};
+	}
 }
-var isOver2 = false; //false 일 때 서브메뉴가 사라짐.
-var isOverSub2 =false; //서브메뉴 1번이 false 일 때 서브메뉴가 사라지는 조건
-// 2번 서브메뉴 숨기기
+var isOver2 = false; // 대메뉴2이 false 일때 서브메뉴가 사라지는 조건
+var isOverSub2 = false; // 서브메뉴2번이 false 일때 서브메뉴가 사라지는 조건
+// 2번 서브메뉴 숨기는 함수(아래)
 function goHide2() {
 	if(!isOver2 && !isOverSub2) {
 		$('.gnb_depth2_2').stop().fadeOut('fast');
 	}
 }
-//pc용 서브메뉴가 나타나는 기능 만들기
+
+//PC용 서브메뉴가 나타나는 액션 만들기(아래)
 $(document).ready(function(){
-	//대메뉴 1번 액션
+	// 대메뉴1 액션
 	$('.openAll1').mouseover(function(){
-		if( parseInt($('header').css('width')) >=1049 ){
-			$('.gnb_depth2_1').stop().fadeIn('fast');
+		if( parseInt($('header').css('width')) >=1049) {
+			$('.gnb_depth2_1').stop().fadeIn('fast');//서브메뉴1번 나타나게 처리
 		}
-		isOver1 = true; //false 일 때 숨기니까 
+		isOver1 = true;// false일때 숨기는 조건 <-> true일때 나타나는 조건
 	});
 	$('.openAll1').mouseout(function(){
 		isOver1 = false;
-		setTimeout('goHide1()', 200); //기본은 goHide1() 만하면 되는데, mouse 아웃 이벤트를 놓칠까봐 0.2초 셋타임아웃 넣어줌
+		setTimeout('goHide1()',200);// 기본은 goHide1() 이것만하면되는데, mouseout이벤트 놓치면 않되기때문에 setTimeout사용
 	});
-	//서브메뉴 1 액션
+	// 서브메뉴1 액션
 	$('.gnb_depth2_1').mouseover(function(){
-		isOverSub1 = true;
+		isOverSub1 = true;//서브메뉴 1 계속 나타납니다.
 	});
 	$('.gnb_depth2_1').mouseout(function(){
-		isOverSub1 = false;
-		setTimeout('goHide1()', 200);
+		isOverSub1 = false;//서브메뉴 1번이 사라집니다.
+		setTimeout('goHide1()',200);
 	});
-	// 대메뉴 2번 액션
+
+	// 대메뉴2 액션
 	$('.openAll2').mouseover(function(){
-		$('.openAll2').mouseover(function(){
-		if( parseInt($('header').css('width')) >=1049 ){
-			$('.gnb_depth2_2').stop().fadeIn('fast');
+		if( parseInt($('header').css('width')) >=1049) {
+			$('.gnb_depth2_2').stop().fadeIn('slow');//서브메뉴1번 나타나게 처리
 		}
-		isOver1 = true; //false 일 때 숨기니까 
-	});
+		isOver2 = true;// false일때 숨기는 조건 <-> true일때 나타나는 조건
 	});
 	$('.openAll2').mouseout(function(){
-		$('.openAll2').mouseout(function(){
-		isOver1 = false;
-		setTimeout('goHide1()', 200); //기본은 goHide1() 만하면 되는데, mouse 아웃 이벤트를 놓칠까봐 0.2초 셋타임아웃 넣어줌
+		isOver2 = false;
+		setTimeout('goHide2()',200);
 	});
-	});
-	//서브메뉴 2 액션
+	// 서브메뉴2 액션
 	$('.gnb_depth2_2').mouseover(function(){
-		isOverSub2 = true;
+		isOverSub2 = true;//서브메뉴 2 계속 나타납니다.
 	});
 	$('.gnb_depth2_2').mouseout(function(){
-		isOverSub2 = false;
-		setTimeout('goHide2()', 200);
-	});	
+		isOverSub2 = false;//서브메뉴 2번이 사라집니다.
+		setTimeout('goHide2()',200);
+	});
 });
